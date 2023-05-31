bd54f4b3bb9b3ac28d505538f707af5d18c87929
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-05-31 20:56:28 +0530

    day 10 completed

---
+++ 09_While_Loops.js
@@ -0,0 +1,16 @@
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+
+// Do while loop
+let j=0;
+do{
+  console.log("Running")
+  console.log(j)
+  j++
+}while(j<n)
