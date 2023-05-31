1d3ba9edf6d7885c97fc123a7b3432f52737797b
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-05-31 20:49:36 +0530

    day 5 completed

---
+++ 04_Practice.js
@@ -0,0 +1,15 @@
+// Create a variable of type string and try to add a number to it.
+let a = "Hello world"
+let b = 10
+console.log(a+b)
+
+// Use typeof operater to find the datatype of the string of last question
+console.log(typeof(a))
+
+// Try to add a new key to the const object
+const object = {
+  name: "Andres",
+  role: "InCharge"
+}
+object["isProffessor"] = false
+console.log(object)
