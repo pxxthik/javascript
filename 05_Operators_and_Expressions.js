1b32ec2c28b9d1f0abfaf28d16c5ca9bd238c37d
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-05-31 20:51:25 +0530

    day 6 completed

---
+++ 05_Operators_and_Expressions.js
@@ -0,0 +1,42 @@
+console.log("Operators in JS")
+
+// Arithmetic Operators
+let a = 45;
+let b = 4;
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a * b = ", a * b)
+console.log("a / b = ", a / b)
+console.log("a % b = ", a % b)
+console.log("a ** b = ", a ** b)
+console.log("a++ = ", a++)
+console.log("a-- = ", a--)
+
+// Assignment Operators
+a = 8
+a += 5 // same as a = a + 5
+console.log(a)
+
+// Comparision Operator
+let comp1 = 6
+let comp2 = 7
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+
+console.log("comp1 == comp2 is ", comp1 === comp2)
+console.log("comp1 != comp2 is ", comp1 !== comp2)
+
+console.log("comp1 > comp2 is ", comp1 > comp2)
+
+// Logical Operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==y)
+console.log(x<y || x==y)
+console.log(!(x<y || x==y))
+console.log(!false)
+
+// single line comment
+/* multi
+line
+comment */
