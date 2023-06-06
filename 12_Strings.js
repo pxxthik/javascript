3eb52cedba3535d814ddc284f203b0013353ae10
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-06 20:18:04 +0530

    day 13 Strings

---
+++ 12_Strings.js
@@ -0,0 +1,16 @@
+let str = "hello world"
+console.log(str.length)
+console.log(str[0])
+
+let friend = 'myfriend'
+
+// Templete literals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+
+// Nikhil is a friend of Pramod
+
+let sentence = `${boy2} is a friend of ${boy2}`
+console.log(sentence)
+
+console.log("hello\" world")
