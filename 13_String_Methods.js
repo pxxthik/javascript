5d8ee26538701c888a7a507e12bea2c194c60609
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-06 20:48:06 +0530

    day 14 String methods

---
+++ 13_String_Methods.js
@@ -0,0 +1,27 @@
+let str = "hello world"
+console.log(str.length)
+
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.slice(2,4))
+console.log(str.slice(2))
+console.log(str.replace("world", "your_name"))
+
+let str1 = "hello"
+let str2 = "world"
+console.log(str1.concat(" ", str2, " OK"))
+
+let name = "     Meena"
+console.log(name)
+console.log(name.trim())
+
+console.log(str[0])
+console.log(str[1])
+console.log(str[2])
+console.log(str[3])
+console.log(str[4])
+
+// QuickQuise
+for(let i=0; i<str.length; i++){
+  console.log(str[i])
+}
