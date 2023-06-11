13c28e006ab9c729b0ff5191a86cf7aab3fecb71
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-11 19:26:51 +0530

    day 25 console and objects in javascript

--- 23_Script_Tag/script.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello")
-
-let a= 8;
-let b= 4;
-console.log((a+b))
---
+++ 24_Console_Objects/script.js
@@ -0,0 +1,39 @@
+console.log("Hello")
+
+let a= 8;
+let b= 4;
+console.log((a+b))
+
+console.log(console)
+
+// throws an error when the condition is false
+console.assert(5>3)
+
+// Clears the console
+console.clear()
+
+let obj = {a: 1, b: 2, c: 3}
+console.table(obj)
+
+console.warn("This is warning")
+console.error("This is an error")
+
+console.info("This is info")
+
+console.table(console)
+
+
+// time
+console.time("forloop")
+for(let i=0; i< 1000; i++){
+  console.log(1000)
+}
+console.timeEnd("forloop")
+
+console.time("whileloop")
+let i=0;
+while(i<1000){
+  console.log(1000)
+  i++;
+}
+console.timeEnd("whileloop")
