a586d9619176916fe7be52d4521e0275562fbd48
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-06-30 12:49:41 +0530

    table navigations in javascript

---
+++ 34_Table_Links/script.js
@@ -0,0 +1,7 @@
+let t = document.body.firstElementChild.firstElementChild
+
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
