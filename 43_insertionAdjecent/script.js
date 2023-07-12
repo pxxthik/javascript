3eb65620e125c6d3d0ff4ed4b5b1427622484e97
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-07-12 19:34:32 +0530

    inserting element through insertAdjecent

---
+++ 43_insertionAdjecent/script.js
@@ -0,0 +1,8 @@
+let first = document.getElementById("first")
+
+first.insertAdjacentHTML("beforeend", "<div class='test'>beforeend</div>")
+first.insertAdjacentHTML("beforebegin", "<div class='test'>beforebegin</div>")
+first.insertAdjacentHTML("afterbegin", "<div class='test'>afterbegin</div>")
+first.insertAdjacentHTML("afterend", "<div class='test'>afterend</div>")
+
+// first.remove()
