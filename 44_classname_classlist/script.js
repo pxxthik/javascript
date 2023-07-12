3e3e36690f5ab15a06343293fe38e2a504664523
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-07-12 22:18:57 +0530

    className and classList

---
+++ 44_classname_classlist/script.js
@@ -0,0 +1,10 @@
+first.className = "yellow class1"
+
+console.log(first.classList)
+
+first.classList.remove("class1")
+first.classList.add("class1")
+
+first.classList.toggle("class1")
+
+console.log(first.classList.contains("class1"))
