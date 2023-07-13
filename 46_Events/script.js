ed26d8ea8deca73a8e0c210d5d62d499bf81b622
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-07-13 19:31:59 +0530

    events in javascript

--- 46_Events/script.js
+++ 46_Events/script.js
@@ -2,3 +2,21 @@ let a = document.getElementById("first")
 a.onclick = ()=>{
   console.log("Clicked")
 }
+
+let x = (e)=>{
+  console.log(e)
+  alert("hello1")
+}
+let y = (e)=>{
+  alert("hello2")
+}
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", x)
+btn.addEventListener("click", y)
+
+let a = prompt("WHat is your faverate number?")
+if(a == "2"){
+  btn.removeEventListener("click", x)
+}
